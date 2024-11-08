d33b2ce73e5bdd45ff0966adb476e23884d1214c
AmitSheoran-557 <amit22082006@gmail.com>
2024-11-08 16:41:49 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+function toggleGrowBox(box) {
+    if (box.classList.contains('grow-box')) {
+        box.classList.remove('grow-box');
+    } else {
+        const boxes = document.querySelectorAll('.inner-box');
+        boxes.forEach(item => item.classList.remove('grow-box'));
+        box.classList.add('grow-box');
+    }
+}
+
+
+
